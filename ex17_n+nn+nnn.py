6326ff96b1e08de387ce05428d6bb1625786afac
Hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2022-11-04 11:19:37 +0530

    Update ex17_n+nn+nnn.py

--- ex17_n+nn+nnn.py
+++ ex17_n+nn+nnn.py
@@ -1,6 +1,6 @@
 #17) Accept an integer N, and compute N + NN + NNN 
 
-a=input("Enter an number: ")
-b=a+a
-c=a+a+a
-print(int(a),"+",int(b),"+",int(c),"=",int(a)+int(b)+int(c))
+a=int(input("Enter an number: "))
+b=a*a
+c=a*a*a
+print(a,"+",b,"+",c,"=",a+b+c)
