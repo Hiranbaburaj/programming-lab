ca789d7f13cb189f2b6db6a65a5c392c60a2b25c
Hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2022-11-25 11:59:31 +0530

    Add files via upload

---
+++ ex27_numberpyramid.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number"))
+'''for i in range (1,n+1):
+    a=1
+    b=n*i
+    a+=1
+    print(b,end=" ")'''
+
+for i in range(1,n+1):
+    for j in range(i,i*i):
+        c=
