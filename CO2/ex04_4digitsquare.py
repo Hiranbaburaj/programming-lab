ed20cd2cc08480b00f4ada332d24b5544b82fd17
Hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2022-12-12 18:29:04 +0530

    Update ex04_4digitsquare.py

--- CO2/ex04_4digitsquare.py
+++ CO2/ex04_4digitsquare.py
@@ -1,38 +1,18 @@
 #Generate a list of four digit numbers in a given range with all their digits even and the number is a perfect square. 
-def perfectsquare(n1,n2):
-    a=[x*x for x in range(32,100) if x%2==0]
-    #print(a)
-    j=len(a)
+def fourdigitsquare(n):
     e=[]
-    for i in range(0,j):
-        number=a[i]
-        d=str(number)
-        c=[x for x in d]
-        if int(c[0])%2==0 and int(c[1])%2==0 and int(c[2])%2==0 and int(c[3])%2==0:
-            e.append(a[i])
-    #print(e)
-    finallist=[]
-
-    if n1 > 9999 or n1 < 1000:
-        print ("entered digit not 4 digit number")
-
-    elif n2 > 9999 or n2 < 1000:
-        print ("entered digit not 4 digit number")
-
-    else:
-        if n1<n2:
-            for i in range(0,4):
-                if e[i]>=n1 and e[i]<=n2:
-                    finallist.append(e[i])
-
-        else:
-            for i in range(0,4):
-                if e[i]>=n2 and e[i]<=n1:
-                    finallist.append(e[i])
-
-        print(finallist)
-
-n1= int(input("enter the 1st 4 digit number: "))
-n2= int(input("enter the 2nd 4 digit number: "))
-
-perfectsquare(n1,n2)
+    for i in range(1000,n):
+        c=[x for x in str(i)]
+        k=[]
+        for j in range(len(c)):
+            if int(c[j])%2==0:
+                k.append(c[j])
+            if len(k) == len(c):
+                if i**0.5 == int(i**0.5):
+                    e.append(i)
+    print(e)
+n=int(input("Enter the maximum range: "))
+if n>=1000:
+    fourdigitsquare(n)
+else:
+    print("Enter a number above 1000")
